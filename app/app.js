f071de14ec2350c7b36386e2c8033011e6c2b215
Georgiana Bere <bere.georgi@gmail.com>
2018-09-06 08:36:53 +0300

    Add buttons to unsubscribe from email and permanently

--- app/app.js
+++ app/app.js
@@ -10,6 +10,79 @@ function displayErr(message) {
   client.interface.trigger('showNotify', { type: 'danger', message: message});
 }
 
+function displaySuc(message) {
+  client.interface.trigger('showNotify', { type: 'success', message: message});
+}
+
+function unsubscribeContact(email) {
+  return new Promise(function(resolve) {
+    const body = {
+      "api_token": '<%= iparam.identity_api_token %>',
+      "email": email,
+      // For now only email
+      "subscription_id": 1,
+    };
+    const uri = '<%= iparam.identity_base_url %>' + '/api/subscriptions/unsubscribe';
+    client.request.post(uri, {
+      uri,
+      body,
+      json: true,
+    })
+      .then(function() {
+        resolve(true);
+      }, function(err) {
+        if (err.status >= 400 && err.status < 500) {
+          displayErr(err.response.error);
+        } else {
+          console.log(err);
+          displayErr('Couldn\'t unsubscribe contact from email.');
+        }
+      });
+  });
+}
+
+function unsubscribePermanently(email) {
+  return new Promise(function(resolve) {
+    const body = {
+      "api_token": '<%= iparam.identity_api_token %>',
+      "email": email,
+    };
+    const uri = '<%= iparam.identity_base_url %>' + '/api/subscriptions/unsubscribe_permanently';
+    client.request.post(uri, {
+      uri,
+      body,
+      json: true,
+    })
+      .then(function() {
+        resolve(true);
+      }, function(err) {
+        if (err.status >= 400 && err.status < 500) {
+          displayErr(err.response.error);
+        } else {
+          console.log(err);
+          displayErr('Couldn\'t unsubscribe contact from email.');
+        }
+      });
+  });
+}
+
+function fetchContactSubscriptions(email) {
+  return new Promise(function(resolve) {
+    const uri = '<%= iparam.identity_base_url %>' + '/api/subscriptions?' +
+                'email=' + email + '&api_token=' + '<%= iparam.identity_api_token %>';
+    client.request.get(uri, {
+      uri,
+      json: true
+    })
+      .then(function(data) {
+        resolve(data.response);
+      }, function(err) {
+        displayErr('Error retrieving subscriptions from Identity');
+        console.log(err);
+      });
+  });
+}
+
 function fetchContactDetails(email) {
   return new Promise(function(resolve) {
     const body = {
@@ -89,7 +162,7 @@ $(document).ready( function() {
         });
     },
 
-    function(crmContactInformation) {
+    function(crmContactInformation, callback) {
       const created_at = moment(crmContactInformation.created_at)
       const now = moment();
       const member_since = moment.duration(now.diff(created_at));
@@ -104,6 +177,58 @@ $(document).ready( function() {
       for (var donationKey in DONATIONS_INFO_MAPPING) {
         displayInfo(DONATIONS_INFO_MAPPING[donationKey], crmContactInformation.donations[donationKey]);
       }
+
+      callback(null, crmContactInformation);
+    },
+
+    function(crmContactInformation, callback) {
+      fetchContactSubscriptions(crmContactInformation.email)
+      .then(function(subscriptions) {
+        if (subscriptions.length > 0) {
+          callback(null, subscriptions, crmContactInformation);
+        }
+        else {
+          jQuery('#contact-info')
+            .append('<div class="fw-content-list"><div class="muted">Contact has no subscriptions.</div></div>');
+          jQuery('#unsubscribe').hide();
+        }
+      });
+    },
+
+    function(subscriptions, crmContactInformation, callback) {
+      const emailSub = subscriptions.find((sub) => sub.subscription_id === 1);
+      if (emailSub === undefined) {
+        jQuery('#contact-info')
+          .append('<div class="fw-content-list"><div class="muted">Contact has no email subscription.</div></div>');
+      } else if(emailSub.unsubscribed_at) {
+        jQuery("#unsub-email").show();
+        jQuery("#unsub-email").attr('disabled', true);
+      } else {
+        jQuery("#unsub-email").show();
+        jQuery("#unsub-email").click(function() {
+          unsubscribeContact(crmContactInformation.email)
+          .then(function(succ) {
+            displaySuc('Successfully unsubscribed contact from email.');
+            jQuery("#unsub-email").attr('disabled', true);
+          });
+        });
+      }
+
+      const permaSub = subscriptions.find((sub) => sub.permanent === true);
+      if(permaSub) {
+        jQuery("#unsub-perma").show();
+        jQuery("#unsub-perma").attr('disabled', true);
+      } else {
+        jQuery("#unsub-perma").show();
+        jQuery("#unsub-perma").click(function() {
+          unsubscribePermanently(crmContactInformation.email)
+          .then(function(succ) {
+            displaySuc('Permanently unsubscribed contact.');
+            jQuery("#unsub-perma").attr('disabled', true);
+          });
+        });
+      }
     }
+
   ]);
 });
